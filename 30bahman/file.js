c47444569c02b1df46e00972bf21dfd40f24e997
mohammadrezaelahi <m.reza.elahi.1378@gmail.com>
2023-02-26 16:14:13 +0330

    create table

--- 30bahman/file.js
+++ 30bahman/file.js
@@ -1,8 +1,21 @@
-a = 10
-let b =[]
-for(a ; a < 20; a++){
-    b.push(a)
+// a = 10
+// let b =[]
+// for(a ; a < 20; a++){
+//     b.push(a)
+// }
+// for(i=0; i<10; i++){
+//     document.write(b[i]+"<br>")
+// }
+
+var result = "<table>";
+
+for (i = 0; i <= 10; i++) {
+  result += "<tr>";
+  for (j = 0; j <= 10; j++) {
+    result += "<td>" + i * j + "</td>";
+  }
+  result += "</tr>";
 }
-for(i=0; i<10; i++){
-    document.write(b[i]+"<br>")
-}
+result += "</table>";
+
+document.getElementById("hi").innerHTML = result;
